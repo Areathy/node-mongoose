d911b75156eece3860acecf11e2caee85b95a3db
hnjoku <hnjoku@parkwayprojects.com>
2021-03-22 08:37:19 +0100

    Mongoose part3

--- index.js
+++ index.js
@@ -9,27 +9,41 @@ connect.then((db) => {
 
     console.log('Connected correctly to server');
 
-    var newDish = Dishes({
+    Dishes.create({
         name: 'Uthappizza',
         description: 'test'
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: "Updated test"},
+        }, { 
+            new : true
+        }).exec();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m Henrietha C.N.',
+            author: 'Henrietha"s"'
+        })
+        
+        return dish.save();
+    })
+
+    .then((dish) =>{
+        console.log(dish);
+
+        return Dishes.remove({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
     });
-
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
-
-            return Dishes.find({});
-        })
-        .then((dishes) => {
-            console.log(dishes);
-
-            return Dishes.remove({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
 
 });
